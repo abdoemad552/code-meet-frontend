91db4ef109cdecc918676c0448c4034c35d0198b
Mostafahassen1 <mostafahass314@gmail.com>
2025-02-11 20:36:04 -0800

    fixed

---
+++ backend/code-meet/src/main/java/com/codemeet/CodeMeetApplication.java
@@ -0,0 +1,13 @@
+package com.codemeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeMeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeMeetApplication.class, args);
+	}
+
+}
---
+++ backend/code-meet/src/test/java/com/codemeet/CodeMeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codemeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeMeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
