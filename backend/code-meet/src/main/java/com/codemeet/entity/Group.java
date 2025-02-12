22d31d71ac741d8b8a360686cb21d2e74bb32991
Mostafahassen1 <mostafahass314@gmail.com>
2025-02-11 22:01:08 -0800

    implement : database

---
+++ backend/code-meet/src/main/java/com/codemeet/entity/Group.java
@@ -0,0 +1,22 @@
+package com.codemeet.entity;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "groups")
+public class Group {
+
+    @Id
+    private Integer id;
+    private String name;
+    private User createdBy;
+    private LocalDateTime createdAt;
+
+
+
+}
