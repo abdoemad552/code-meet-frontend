5e555ecd743de90e2c5b76622c9675b59d6a5562
Ahmed Montaser <ahmedalmaster2@gmail.com>
2025-03-02 23:47:52 +0200

    Changed the appearance of friends list cards.

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -8,6 +8,8 @@ import {getRoutes} from './app.routes';
 // We need to check authentication first and after the user is authenticated the
 // getRoutes(isLoggedIn : boolean) will take a flag that indicates the authentication state and updates the routes.
 
+let isUserAuthenticated : boolean = true;
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(getRoutes(true))]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(getRoutes(isUserAuthenticated))]
 };
