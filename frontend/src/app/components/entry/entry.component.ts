378feaf7a745f572b268b27023fc4e811684d881
abdoemad552 <abdoemad552@gmail.com>
2025-06-09 21:37:04 +0300

    Enhanced Entry page and App logo

--- frontend/src/app/components/entry/entry.component.ts
+++ frontend/src/app/components/entry/entry.component.ts
@@ -1,18 +1,21 @@
-import { NgClass } from '@angular/common';
+import {NgClass, NgIf} from '@angular/common';
 import { Component } from '@angular/core';
 import {Router, RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-entry',
-  imports: [RouterLink, RouterLinkActive, NgClass],
+  imports: [RouterLink, RouterLinkActive, NgClass, NgIf],
   templateUrl: './entry.component.html',
   styleUrl: './entry.component.css'
 })
 export class EntryComponent {
-  mainFeatures : string[] = ["Interactive code meetings with your colleuges with an embedded code editor and voice sharing.", "Rooms to share messages with your friends."];
+  mainFeatures : string[] = [
+    "Interactive code meetings with your colleges with an embedded code editor and voice sharing.",
+    "Rooms to share messages with your friends."
+  ];
 
-  isMainFeaturesHidden = true;
+  isMainFeaturesHidden = false;
 
   constructor(private router: Router) {
   }
